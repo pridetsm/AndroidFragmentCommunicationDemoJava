1a6c2cd6d1a9b4f7f5443781f6e496c2b0e90dbd
Moyo Pride ST <50073776+prido9k@users.noreply.github.com>
2020-08-09 20:29:51 +0200

    Added comments

--- app/src/main/java/com/callbackinterfaces/androidfragmentcommunicationdemojava/MainActivity.java
+++ app/src/main/java/com/callbackinterfaces/androidfragmentcommunicationdemojava/MainActivity.java
@@ -1,3 +1,4 @@
+/*Written by:Pride Moyo ST (09/08/2020)*/
 package com.callbackinterfaces.androidfragmentcommunicationdemojava;
 
 import android.graphics.Color;
@@ -11,30 +12,37 @@ public class MainActivity extends AppCompatActivity implements FirstFragment.OnF
     private FirstFragment firstFragment;
     private SecondFragment secondFragment;
     private FragmentManager fm;
-   private Toolbar toolbar;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initializing our toolbar
         toolbar=findViewById(R.id.toolbar);
         toolbar.setTitleTextColor(Color.WHITE);
+        //setting our toobar as our application's appbar
         setSupportActionBar(toolbar);
+        
         fm=getSupportFragmentManager();
+        //NB:This app is not meant to survive any configuration so just this simple assignment will do for fragments.
         firstFragment=new FirstFragment();
         secondFragment=new SecondFragment();
         fm.beginTransaction().replace(R.id.fragmentContainer,firstFragment)
         .commit();
     }
-
+    //handling the onFirstFragTextSent event 
     @Override
     public void onFirstFragTextSent(String text) {
+        //we create a bundle and attach our data to.
         Bundle bundle=new Bundle();
         bundle.putString("RECEIVED",text);
+        //then we set that bundle as the arguments of our fragment
         secondFragment.setArguments(bundle);
         fm.beginTransaction().replace(R.id.fragmentContainer,secondFragment)
                 .commit();
     }
+    //handling the onSecondFragTextSent event
     @Override
     public void onSecondFragTextSent(String text) {
         Bundle bundle=new Bundle();
@@ -43,6 +51,7 @@ public class MainActivity extends AppCompatActivity implements FirstFragment.OnF
         fm.beginTransaction().replace(R.id.fragmentContainer,firstFragment)
                 .commit();
     }
+    //this method is used by the fragments on navigation to show which fragment is currently resumed.
     public void setActionBarTitle(String string) {
         getSupportActionBar().setTitle(string);
     }
